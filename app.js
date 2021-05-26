aa3cf22884f26979728c914aee10284f4742f376
Anton Shishenya <anton.shishenya@gmail.com>
2021-05-26 19:45:39 +0300

    Major changes

--- app.js
+++ app.js
@@ -3,19 +3,36 @@ const fs = require("fs");
 const cors = require('cors');
 const app = express();
 const jsonParser = express.json();
+const MongoClient = require("mongodb").MongoClient; // импортируем из библиотеки MongoClient
 
 app.use(cors());
 app.use(express.static(__dirname + "/public"));
 
 const filePath = "appointments.json";
 
+let dbClient; // объявляем dbClient, через который
+let db;
+let collection;
+
+// создаем объект mongoClient и передаем ему строку подключения
+const mongoClient = new MongoClient("mongodb://localhost:27017/", { useUnifiedTopology: true });
+mongoClient.connect((err, client) => {
+    if (err) {
+        return console.log("Ошибка подключения к Mongo", err);
+    }
+    dbClient = client; // чтобы можно было использовать dbClient за пределами этого коллбэка
+    db = dbClient.db("mrs");
+    collection = db.collection("appointments");
+    // взаимодействие с базой данных
+    // client.close(); // пока не закрываем подключение к базе
+});
+
 //получение всех пользователей
 app.get("/api/appointments", function (req, res) {
-
-    const content = fs.readFileSync(filePath, "utf8");
-
-    const appointments = JSON.parse(content);
-    res.send(appointments);
+    collection.find({}).toArray((err, appointments) => {
+        if (err) return console.log("Ошибка получения записей приёмов: ", err); // В случае ошибки ругаемся и выходим из функции
+        res.send(appointments); // В случае успеха, отправляем полученные данные на фронт
+    });
 });
 
 // получение одного пользователя по id
